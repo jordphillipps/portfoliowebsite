b5c708f29e01393ef695215b49767e5fa8999ac1
Jordan Phillipps <Jordan1@MacBook-Pro.home>
2016-08-24 14:25:52 +0100

    initila commit

---
+++ resources/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+	$('.js--section-features').waypoint(function(direction) {
+
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px;'
+
+	});
+
+
+
+/* mobile navigation */
+
+
+$('.js--nav-icon').click(function(){
+
+	var nav = $('.js--main-nav');
+	var icon = $('.js--nav-icon i');
+
+	nav.slideToggle(200);
+	if (icon.hasClass('ion-navicon-round')) {
+		icon.addClass('ion-close-round');
+		icon.removeClass('ion-navicon-round');
+
+	} else {
+		icon.addClass('ion-navicon-round');
+		icon.removeClass('ion-close-round');
+	}
+});
+
+});
